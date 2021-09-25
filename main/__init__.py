521740e226024d32395c8330d8142d1da764221b
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-25 16:23:24 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,10 +1,1 @@
-from main.config import Config
-from pyrogram import Client as kabeercmd
 
-log_channel_ = Config.LOG_CHANNEL
-
-async def main_startup():
-  await kabeercmd.send_message(chat_id=log_channel_id, text="`Nexa Userbot is started!`")
-  await idle()
-  
-print("Bot Started")
