5a107830e99c25c0f3aade227c57f2457cc75c33
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-25 15:10:16 +0530

    Create start.py

---
+++ main/pl/start.py
@@ -0,0 +1,21 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import Config
+from pyrogram import Client as kabeercmd
+import text
+
+@kabeercmd.on_message(filters.command('start') & filters.private)
+async def start(client, message):
+    await message.reply_text(
+        text=text.START_MSG,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Source", url="https://github.com/vivek-tp/Tg-Bot"),
+                    InlineKeyboardButton("Support", url="https://t.me/OpensourceTG")
+                ]
+            ]
+        ),
+        reply_to_message_id=message.message_id
+    )
