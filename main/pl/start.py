b941e5722c2a8ed72411244f1f3a235f3397087a
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-25 16:19:02 +0530

    Update start.py

--- main/pl/start.py
+++ main/pl/start.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from main.config import Config
 from pyrogram import Client 
 
-@Client.on_message(filters.command('start') & filters.private)
+@Client.on_message(filters.command('start'))
 async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""Bot Under Beta Testing"""
