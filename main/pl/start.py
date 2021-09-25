0e7a0e9508ee54fd69194a3ccde986604a2baf2d
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-25 16:17:42 +0530

    Update start.py

--- main/pl/start.py
+++ main/pl/start.py
@@ -1,11 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from main.config import Config
-from pyrogram import Client as kabeercmd
-from main.text import MSG
+from pyrogram import Client 
 
-@kabeercmd.on_message(filters.command('start') & filters.private)
-async def start_(client: kabeercmd, message: Message):
+@Client.on_message(filters.command('start') & filters.private)
+async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""Bot Under Beta Testing"""
         disable_web_page_preview=True,
