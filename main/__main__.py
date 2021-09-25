5d5b5e97674fd1b835ef3c11f2e1a12bae53838b
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-25 16:05:14 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1,6 +1,6 @@
 from main.config import Config
 
-from pyrogram import Client as kabeercmd
+from pyrogram import Client
 
 kabeercmd = Client("TG-Bot", bot_token = Config.BOT_TOKEN, api_id = Config.API_ID, api_hash = Config.API_HASH)
 
