81dfb70e64e4c40b92c8b30c95b3355d70e98a10
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-25 16:09:21 +0530

    Update config.py

--- main/config.py
+++ main/config.py
@@ -4,8 +4,8 @@ import os
 
 class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    APP_ID = int(os.environ.get("APP_ID"))
-    APP_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     OWNER_ID = int(os.environ.get("OWNER_ID"))
     SUDO_USERS = int(os.environ.get("SUDO_USERS"))
@@ -20,7 +20,7 @@ class Local:
     
     LOG_CHANNEL = "-1001182935463"
     BOT_TOKEN = "123456:jsd9rh443fh9dvs"
-    APP_ID = "192803"
+    API_ID = "192803"
     API_HASH = "hd82828h2872tw02"
     BOT_USERNAME = "MissTanuBot"
     OWNER_ID = "1182935463"
